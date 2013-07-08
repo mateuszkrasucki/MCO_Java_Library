b8f534b6c319ad3041ae12481028522a86c93b9a
Mateusz Krasucki <mateuszkrasucki@Probook.local>
2013-07-08 16:39:44 +0200

    Poprawki w Electre.

--- MCO_Java_Library/src/methods/Electre/Electre1.java
+++ MCO_Java_Library/src/methods/Electre/Electre1.java
@@ -4,8 +4,11 @@
  */
 package methods.Electre;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -13,145 +16,278 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-import methods.Electre.Criterium.Direction;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 
+
 public class Electre1 {
 
+	public enum Direction {
+		MIN, MAX
+	}
 	private RealMatrix concordanceMatrix_;
-	private RealMatrix discordanceMatrix_;
+	private RealMatrix discordanceMatrix_;	
 	private RealMatrix normalized_;
-	private Double p_;
-	private Double q_;
 	private LinkedList<Double[]> alternatives_;
-	private LinkedList<Criterium> criteria_;
+	private LinkedList<Electre1.Direction> directions_;
+	private LinkedList<Double> weights_;
 	private Integer[] ranking_;
 	private Integer[] rankingPoints_;
+	private int altCount_;
+	private Double s_;
+	private Double veto_;
 	
-	public Electre1(Data data, Double p, Double q) {
-		alternatives_=data.alternatives_;
-		criteria_=data.criteria_;
-		concordanceMatrix_ = new Array2DRowRealMatrix(new double[alternatives_.size()][alternatives_.size()]);
-		discordanceMatrix_ = new Array2DRowRealMatrix(new double[alternatives_.size()][alternatives_.size()]);
-		normalized_ = new Array2DRowRealMatrix(new double[alternatives_.size()][criteria_.size()]);
-		ranking_ = new Integer[alternatives_.size()];
-		rankingPoints_ = new Integer[alternatives_.size()];
-		p_=p;
-		q_=q;
-	}
+	/**
+	* 
+	* Cosntructor for Electre1 method
+	* @param filename Filename where data can be read from
+	* @param s a double value of threshold used for concordance test (should be over 0.5)
+	* @param veto a double value of threshold used for disconcordance test
+	*/
+	public Electre1(String filename, Double s, Double veto) {
+		
+		directions_ = new LinkedList<Electre1.Direction>();
+		weights_ = new LinkedList<Double>();
+		alternatives_ = new LinkedList<Double[]>();
+		s_=s;
+		veto_=veto;
+		
+		BufferedReader br = null;
+		String line = "";
+		altCount_ = 0;
+		
+		try {
+			br = new BufferedReader(new FileReader(filename));
+			int i=0;
+			while ((line = br.readLine()) != null) {
+				
+				String[] criterium = line.split(";");
+				
+				if (i==0) {
+					altCount_=criterium.length-2;
+				}
+				// the same amount of criteria in each alternative
+				else if (altCount_!=criterium.length-2)
+					throw new Exception();
+				
+				if (criterium[0].compareToIgnoreCase("max")==0)
+					directions_.add(Electre1.Direction.MAX);
+				else
+					directions_.add(Electre1.Direction.MIN);
+				
+				weights_.add(Double.valueOf(criterium[1]));
+				alternatives_.add(new Double[altCount_]);
+				
+				for (int j=2; j<criterium.length; j++) {
+					alternatives_.get(i)[j-2] = Double.valueOf(criterium[j]);
+				}
+				i++;
+			}
+			br.close();
+		}
+			catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		concordanceMatrix_ = new Array2DRowRealMatrix(new double[altCount_][altCount_]);
+		discordanceMatrix_ = new Array2DRowRealMatrix(new double[altCount_][altCount_]);
+		normalized_ = new Array2DRowRealMatrix(new double[weights_.size()][altCount_]);
+		ranking_ = new Integer[altCount_];
+		rankingPoints_ = new Integer[altCount_];
+		}
+	
+	
+	/**
+	 * Ranking list getter
+	 * @return Integer array of alternatives starting from the best one
+	 */
 	public Integer[] getRanking() {
 		return ranking_;
 	}
 
+	/**
+	 * Ranking points list getter
+	 * @return Integer array of ranking points of each alternative
+	 */
 	public Integer[] getRankingPoints_() {
 		return rankingPoints_;
 	}
+	
+
+	/**
+	 * Public method calculating all the matrices and filling the ranking lists
+	 */
 	public void calculate() {
 		calculate_concordance_matrix();
 		normalize();
-		calculate_disconcordance_matrix();
 		calculate_ranking();
 	}
+
+	/**
+	 * Helper method
+	 * @param a alternative a index
+	 * @param b alternative b index
+	 * @param criterium Criterium index
+	 * @return bool value whether is alternative a better than alternative b
+	 */
+	private Boolean is_alternative_preferred(int a, int b, int criterium) {
+		if (directions_.get(criterium) == Electre1.Direction.MAX) 
+			return (alternatives_.get(criterium)[a] >= alternatives_.get(criterium)[b]);
+		
+		else return (alternatives_.get(criterium)[a] <= alternatives_.get(criterium)[b]);
+	}
+
+	/**
+	 * Helper method
+	 * @param a alternative a index
+	 * @param b alternative b index
+	 * @return concordance value of alternative a over alternative b
+	 */
 	private double concordance_index(int a, int b) {
 		  double w = 0;
-	        for (int i=0; i<criteria_.size(); i++)
+	        for (int i=0; i<weights_.size(); i++)
 	        	if (is_alternative_preferred(a, b, i))
-	            	w += criteria_.get(i).treshold;
-
+	            	w += weights_.get(i);
 	        return w;
 	  }
-
-	public void calculate_concordance_matrix() {
+	/**
+	 * Method calculating corcondance matrix for every pair of alternatives
+	 */
+	private void calculate_concordance_matrix() {
 		
-		for (int i=0; i< alternatives_.size(); i++)
-			 for (int j=0; j<alternatives_.size(); j++) 
+		for (int i=0; i< altCount_; i++)
+			 for (int j=0; j<altCount_; j++) 
 				 if (i!=j) 
 					 concordanceMatrix_.setEntry(i, j, concordance_index(i, j));
 				 else 
 					 concordanceMatrix_.setEntry(i, j, 0);
 		
 	}
-	
+/**
+ * Method preparing the matrix with normalized values of all alternatives, for calculating discordance factor
+ */
 	public void normalize() {
-		for (int i=0; i<alternatives_.size(); i++)
-			for (int j=0; j<criteria_.size(); j++)
-				normalized_.setEntry(i, j, criteria_.get(j).treshold * alternatives_.get(i)[j] / Math.sqrt(sum_of_sq_for_crit(j)));
+		
+		for (int cri=0; cri<weights_.size(); cri++)
+			for (int alt=0; alt<altCount_; alt++)
+				normalized_.setEntry(cri, alt, weights_.get(cri) * alternatives_.get(cri)[alt] / Math.sqrt(sum_of_sq_for_crit(cri)));
 		
 	}
 	
-	 private double sum_of_sq_for_crit(int crIndex) {
+	 private double sum_of_sq_for_crit(int cri) {
 		 double sum=0;
-		 for (int i=0; i<alternatives_.size(); i++) {
-			 sum += alternatives_.get(i)[crIndex]*alternatives_.get(i)[crIndex];
+		 for (int i=0; i<altCount_; i++) {
+			 sum += alternatives_.get(cri)[i]*alternatives_.get(cri)[i];
 		 }
 		 return sum;
 	 }
-	 
-	private double discordance_index(int a, int b) {
-		Double[] tempList = new Double[criteria_.size()];
-		for (int i=0; i<criteria_.size(); i++) {
-			if (is_alternative_preferred(a, b, i)) 
-				tempList[i] = Math.abs(normalized_.getEntry(a, i) - normalized_.getEntry(b, i));
-			else
-				tempList[i]=(double) 0;
-		}
-		return Collections.max(Arrays.asList(tempList));
-	  }
-	
-	private void calculate_disconcordance_matrix() {
-		
-		for (int i=0; i< alternatives_.size(); i++)
-			 for (int j=0; j<alternatives_.size(); j++) 
-				 if (i!=j) 
-					 discordanceMatrix_.setEntry(i, j, discordance_index(i, j));
-				 else 
-					 discordanceMatrix_.setEntry(i, j, 0);
-		}
-	
-	private void calculate_ranking() {
-		
-		int[][] rankIndex = new int[alternatives_.size()][alternatives_.size()];
 
-		for (int i=0; i< alternatives_.size(); i++)
-			 for (int j=0; j<alternatives_.size(); j++) 
-				 if ((i != j) && (concordanceMatrix_.getEntry(i, j) >= p_) && (discordanceMatrix_.getEntry(i, j) <= q_))
-					 rankIndex[i][j]=1;
-				 else
-					 rankIndex[i][j]=0;
-		
-		HashMap<Integer,Integer> rankMap = new HashMap<Integer,Integer>();
-		for (int j=0; j< alternatives_.size(); j++)
-			{
-			rankMap.put(j+1, 0);
-			for (int i=0; i<alternatives_.size(); i++) {
-				rankMap.put(j+1, rankMap.get(j+1)+rankIndex[i][j]);
+	/**
+	 * Discordance value for a given pair of alternatives
+	 * @param a alternative a index
+	 * @param b alternative b index
+	 * @return discordance value of alternative a with alternative b
+	 */
+	private double discordance_index(int a, int b) {
+		Double max = 0.0;
+		for (int i=0; i<weights_.size(); i++) {
+			if (is_alternative_preferred(b, a, i)) {
+				
+				
+				Double diff = Math.abs(normalized_.getEntry(i, b) - normalized_.getEntry(i, a));
+				if (diff > max)
+				max = diff;
 			}
 		}
+		return max;
+	  }
+//
+//	private void calculate_discordance_matrix() {
+//		
+//		for (int i=0; i< altCount_; i++)
+//			 for (int j=0; j<altCount_; j++) 
+//				 if (i!=j) 
+//					 discordanceMatrix_.setEntry(i, j, discordance_index(i, j));
+//				 else 
+//					 discordanceMatrix_.setEntry(i, j, 0);
+//		
+//		for (int i=0; i<altCount_; i++){
+//			for (int j=0; j<altCount_; j++)
+//				System.out.print(discordanceMatrix_.getEntry(i, j)+" ");
+//			System.out.println();
+//		}
+//		
+//		}
 
+	
+	/**
+	 * Method conducting concordance and discordance test for every pair of alternatives, 
+	 * putting the results into ranking matrix and sorting it by ranking points amount
+	 */
+	private void calculate_ranking() {
+		System.out.println(altCount_);
+		Integer[][] rankingM= new Integer[altCount_][altCount_];
+		
+		for (int a=0; a<altCount_; a++) {
+			for (int b=0; b<altCount_; b++){
+				if (a!=b) {
+					//concordance test
+					if ((concordanceMatrix_.getEntry(a, b) >= s_) && (discordance_index( a,  b) <= veto_))
+					{
+						rankingM[a][b]=1;
+					}
+					else 
+						rankingM[a][b]=0;
+				}
+				else 
+					rankingM[a][b]=0;
+			}
+		}
+		for (int i=0; i<altCount_; i++){
+			for (int j=0; j<altCount_; j++)
+				System.out.print(rankingM[i][j]+" ");
+			System.out.println();
+		}
+		
+		//preparing map containing order number and ranking points
+		HashMap<Integer,Integer> rankMap = new HashMap<Integer,Integer>();
+		for (int a=0; a< altCount_; a++)
+			{
+			rankMap.put(a+1, 0);
+			for (int i=0; i<altCount_; i++) {
+				rankMap.put(a+1, rankMap.get(a+1)+rankingM[a][i]);
+			}
+		}
+		//sorting map by values - ranking points
         LinkedHashMap<Integer,Integer> sorted_map = sortHashMapByValuesD(rankMap);
-        ranking_=sorted_map.keySet().toArray(new Integer[alternatives_.size()]);
-        rankingPoints_ = sorted_map.values().toArray(new Integer[alternatives_.size()]);
+        ranking_=sorted_map.keySet().toArray(new Integer[altCount_]);
+        rankingPoints_ = sorted_map.values().toArray(new Integer[altCount_]);
 		
 	}
 	
-	private Boolean is_alternative_preferred(int a, int b, int criterium) {
-		
-		if (criteria_.get(criterium).direction == Direction.MAX) 
-			return (alternatives_.get(a)[criterium] >= alternatives_.get(b)[criterium]);
-		
-		else return (alternatives_.get(a)[criterium] <= alternatives_.get(b)[criterium]);
-				
-	}
-	
-	public LinkedHashMap<Integer, Integer> sortHashMapByValuesD(HashMap<Integer, Integer> passedMap) {
+
+	/**
+	 * Method used for sorting maps by value
+	 * @param passedMap Integer map to sort by value
+	 * @return Integer map sorted by value
+	 */
+	private LinkedHashMap<Integer, Integer> sortHashMapByValuesD(HashMap<Integer, Integer> passedMap) {
 	    List<Integer> mapKeys = new ArrayList<Integer>(passedMap.keySet());
 	    List<Integer> mapValues = new ArrayList<Integer>(passedMap.values());
 	    Collections.sort(mapValues);
 	    Collections.sort(mapKeys);
 	    Collections.reverse(mapValues);
-
+	
 	    LinkedHashMap<Integer, Integer> sortedMap = 
 	        new LinkedHashMap<Integer, Integer>();
 	    
--- MCO_Java_Library/src/methods/Electre/ElectreIs.java
+++ MCO_Java_Library/src/methods/Electre/ElectreIs.java
@@ -2,8 +2,13 @@
  *
  * @author Gabriela Pastuszka
  */
+
 package methods.Electre;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -12,133 +17,331 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-import methods.Electre.Criterium.Direction;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 
+/**
+ *
+ * @author Mateusz Krasucki, Gabriela Pastuszka
+ */
 public class ElectreIs {
 
+	public enum Direction {
+		MIN, MAX
+	}
 	private RealMatrix concordanceMatrix_;
-	private RealMatrix discordanceMatrix_;
-	private Double s_;
+	private RealMatrix discordanceMatrix_;	
+	private RealMatrix normalized_;
 	private LinkedList<Double[]> alternatives_;
-	private LinkedList<Criterium> criteria_;
+	private LinkedList<ElectreIs.Direction> directions_;
+	private LinkedList<Double> weights_;
+	private LinkedList<Double> vetos_;
+	private LinkedList<Double> p_;
+	private LinkedList<Double> q_;
 	private Integer[] ranking_;
 	private Integer[] rankingPoints_;
+	private int altCount_;
+	private Double s_;
 	
-	public ElectreIs(Data data, Double s) {
-		alternatives_=data.alternatives_;
-		criteria_=data.criteria_;
-		concordanceMatrix_ = new Array2DRowRealMatrix(new double[alternatives_.size()][alternatives_.size()]);
-		discordanceMatrix_ = new Array2DRowRealMatrix(new double[alternatives_.size()][alternatives_.size()]);
-		ranking_ = new Integer[alternatives_.size()];
-		rankingPoints_ = new Integer[alternatives_.size()];
+	/**
+	* 
+	* Cosntructor for Electre1 method
+	* @param filename Filename where data can be read from
+	* @param s a double value of threshold used for concordance test (should be over 0.5)
+	* @param veto a double value of threshold used for disconcordance test
+	*/
+	public ElectreIs(String filename, Double s) {
+		
+		directions_ = new LinkedList<ElectreIs.Direction>();
+		weights_ = new LinkedList<Double>();
+		alternatives_ = new LinkedList<Double[]>();
+		vetos_ = new LinkedList<Double>();
+		p_ = new LinkedList<Double>();
+		q_ = new LinkedList<Double>();
 		s_=s;
-	}
+		
+		BufferedReader br = null;
+		String line = "";
+		altCount_ = 0;
+		
+		try {
+			br = new BufferedReader(new FileReader(filename));
+			int i=0;
+			while ((line = br.readLine()) != null) {
+				
+				String[] criterium = line.split(";");
+
+				if (i==0) {
+					altCount_=criterium.length-5;
+				}
+				// the same amount of criteria in each alternative
+				else if (altCount_!=criterium.length-5) {
+					System.out.println(i);
+					System.out.println(criterium.length);
+					throw new Exception();
+				}
+				if (criterium[0].compareToIgnoreCase("max")==0)
+					directions_.add(ElectreIs.Direction.MAX);
+				else
+					directions_.add(ElectreIs.Direction.MIN);
+				
+				weights_.add(Double.valueOf(criterium[1]));
+				vetos_.add(Double.valueOf(criterium[2]));
+				q_.add(Double.valueOf(criterium[3]));
+				p_.add(Double.valueOf(criterium[4]));
+				
+				if (q_.getLast() > p_.getLast()) {
+					System.out.println(i);
+					throw new Exception();
+				}
+				alternatives_.add(new Double[altCount_]);
+				
+				for (int j=5; j<criterium.length; j++) {
+					alternatives_.get(i)[j-5] = Double.valueOf(criterium[j]);
+				}
+				i++;
+			}
+			br.close();
+		}
+			catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		concordanceMatrix_ = new Array2DRowRealMatrix(new double[altCount_][altCount_]);
+		discordanceMatrix_ = new Array2DRowRealMatrix(new double[altCount_][altCount_]);
+		normalized_ = new Array2DRowRealMatrix(new double[weights_.size()][altCount_]);
+		ranking_ = new Integer[altCount_];
+		rankingPoints_ = new Integer[altCount_];
+		
+		
+		
+		for (int i=0; i<weights_.size(); i++)
+			System.out.println(alternatives_.get(i)[0]);
+		}
+	
+	
+	/**
+	 * Ranking list getter
+	 * @return Integer array of alternatives starting from the best one
+	 */
 	public Integer[] getRanking() {
 		return ranking_;
 	}
 
+	/**
+	 * Ranking points list getter
+	 * @return Integer array of ranking points of each alternative
+	 */
 	public Integer[] getRankingPoints_() {
 		return rankingPoints_;
 	}
 	
+
+	/**
+	 * Public method calculating all the matrices and filling the ranking lists
+	 */
 	public void calculate() {
 		calculate_concordance_matrix();
-		calculate_disconcordance_matrix();
 		calculate_ranking();
 	}
+
+	/**
+	 * Helper method
+	 * @param a alternative a index
+	 * @param b alternative b index
+	 * @return concordance value of alternative a over alternative b
+	 */
 	private double concordance_index(int a, int b) {
-		  double s = 0;
-	        for (int i=0; i<criteria_.size(); i++)
-	        	if (criteria_.get(i).direction==Direction.MAX) {
-	            	if ( alternatives_.get(b)[i] >= alternatives_.get(a)[i])
-	            			s += criteria_.get(i).treshold;
+		  double cIndex = 0;
+		  
+	        for (int i=0; i<weights_.size(); i++) {
+	        	if (directions_.get(i)==ElectreIs.Direction.MAX) {
+	        		if (normalized_.getEntry(i,a) + q_.get(i) >= normalized_.getEntry(i,b))
+	        			cIndex+=weights_.get(i);
+	        	}
+	        	else 
+        		if (normalized_.getEntry(i,a) - q_.get(i) <= normalized_.getEntry(i,b))
+        			cIndex+=weights_.get(i);
+	        }
+
+	        for (int i=0; i<weights_.size(); i++) {
+
+	        	if (directions_.get(i)==ElectreIs.Direction.MAX) {
+	        		if (normalized_.getEntry(i,a) < normalized_.getEntry(i,b) + p_.get(i)) {
+	        			Double fi= ( normalized_.getEntry(i,a) + p_.get(i) - normalized_.getEntry(i,b) ) / ( p_.get(i) - q_.get(i) ); 
+	        			System.out.println(fi);
+	        			cIndex += fi*weights_.get(i);
+	        		}
 	        	}
 	        	else {
-	            	if ( alternatives_.get(b)[i] <= alternatives_.get(a)[i])
-	            			s += criteria_.get(i).treshold;
+	        		if (normalized_.getEntry(i,a) > normalized_.getEntry(i,b) - p_.get(i)) {
+	        			Double fi= ( normalized_.getEntry(i,a) + p_.get(i) - normalized_.getEntry(i,b) ) / ( p_.get(i) - q_.get(i) ); 
+	        			System.out.println(fi);
+	        			cIndex += fi*weights_.get(i);
+	        		}
 	        	}
-	        return s;
+	        }
+	        
+	  return cIndex;
 	  }
-
-	public void calculate_concordance_matrix() {
+	/**
+	 * Method calculating corcondance matrix for every pair of alternatives
+	 */
+	private void calculate_concordance_matrix() {
 		
-		for (int i=0; i< alternatives_.size(); i++)
-			 for (int j=0; j<alternatives_.size(); j++) 
+		for (int i=0; i< altCount_; i++)
+			 for (int j=0; j<altCount_; j++) 
 				 if (i!=j) 
 					 concordanceMatrix_.setEntry(i, j, concordance_index(i, j));
 				 else 
 					 concordanceMatrix_.setEntry(i, j, 0);
-
+		
+//		for (int i=0; i<altCount_; i++){
+//			for (int j=0; j<altCount_; j++)
+//				System.out.print(concordance_index(i, j)+" ");
+//			System.out.println();
+//		}
 	}
-	
-	private double discordance_index(int a, int b) {
-		double m = 1;
-		for (int i=0; i<criteria_.size(); i++) {
-			double v = alternatives_.get(b)[i]*0.4;
-			if (criteria_.get(i).direction==Direction.MAX) {
-            	if ( alternatives_.get(a)[i] >= alternatives_.get(b)[i] + v)
-            			m *= 0;
-        	}
-        	else {
-            	if ( alternatives_.get(a)[i] <= alternatives_.get(b)[i] - v)
-            			m *= 0;
-        	}
+/**
+ * Method preparing the matrix with normalized values of all alternatives, for calculating discordance factor
+ */
+	public void normalize() {
+		
+		for (int cri=0; cri<weights_.size(); cri++)
+			for (int alt=0; alt<altCount_; alt++)
+				normalized_.setEntry(cri, alt, weights_.get(cri) * alternatives_.get(cri)[alt] / Math.sqrt(sum_of_sq_for_crit(cri)));
+		
+		 
+			for (int cri=0; cri<weights_.size(); cri++) {
+				for (int alt=0; alt<altCount_; alt++)
+				System.out.print(normalized_.getEntry(cri,alt)+" ");
+			System.out.println();
 		}
-		return 1 - m;
-	  }
-	
-	private void calculate_disconcordance_matrix() {
 		
-		for (int i=0; i< alternatives_.size(); i++)
-			 for (int j=0; j<alternatives_.size(); j++) 
-				 if (i!=j) 
-					 discordanceMatrix_.setEntry(i, j, discordance_index(i, j));
-				 else 
-					 discordanceMatrix_.setEntry(i, j, 0);
 		
-		}
-	
-	private void calculate_ranking() {
-		
-		int[][] rankIndex = new int[alternatives_.size()][alternatives_.size()];
+//		for (int i=0; i<weights_.size(); i++){
+//			for (int j=0; j<altCount_; j++)
+//				System.out.print(alternatives_.get(i)[j]+" ");
+//			System.out.println();
+//		}
+	}
+//	
+	 private double sum_of_sq_for_crit(int cri) {
+		 double sum=0;
+		 for (int i=0; i<altCount_; i++) {
+			 sum += alternatives_.get(cri)[i]*alternatives_.get(cri)[i];
+		 }
+		 return sum;
+	 }
 
-		for (int i=0; i< alternatives_.size(); i++)
-			 for (int j=0; j<alternatives_.size(); j++) 
-				 if ((i != j) && (concordanceMatrix_.getEntry(i, j) >= s_) && (discordanceMatrix_.getEntry(i, j) == 0))
-					 rankIndex[i][j]=1;
-				 else
-					 rankIndex[i][j]=0;
-//		for (int i=0; i< alternatives_.size(); i++){
-//			 for (int j=0; j<alternatives_.size(); j++) 
-//				 	System.out.print(rankIndex[i][j]+ " ");
-//			 	System.out.println();
+	/**
+	 * Discordance value for a given pair of alternatives
+	 * @param a alternative a index
+	 * @param b alternative b index
+	 * @return discordance value of alternative a with alternative b
+	 */
+	private boolean discordance_condition(int a, int b) {
+		
+		boolean rejected = false;
+		
+		for (int cri =0; cri<weights_.size(); cri++) {
+			
+			Double eta = (1 - concordanceMatrix_.getEntry(a, b) - weights_.get(cri)) / (1 - s_ - weights_.get(cri));
+			
+			if (alternatives_.get(cri)[a] + vetos_.get(cri) < alternatives_.get(cri)[b] + q_.get(cri)*eta)
+				rejected = true;
+		}
+		
+		return rejected;
+		
+	  }
+//
+//	private void calculate_discordance_matrix() {
+//		
+//		for (int i=0; i< altCount_; i++)
+//			 for (int j=0; j<altCount_; j++) 
+//				 if (i!=j) 
+//					 discordanceMatrix_.setEntry(i, j, discordance_index(i, j));
+//				 else 
+//					 discordanceMatrix_.setEntry(i, j, 0);
+//		
+//		for (int i=0; i<altCount_; i++){
+//			for (int j=0; j<altCount_; j++)
+//				System.out.print(discordanceMatrix_.getEntry(i, j)+" ");
+//			System.out.println();
 //		}
 //		
-		HashMap<Integer,Integer> rankMap = new HashMap<Integer,Integer>();
-		for (int i=0; i< alternatives_.size(); i++)
-			{
-			rankMap.put(i+1, 0);
-			for (int j=0; j<alternatives_.size(); j++) {
-				rankMap.put(i+1, rankMap.get(i+1)+rankIndex[i][j]);
+//		}
+
+	
+	/**
+	 * Method conducting concordance and discordance test for every pair of alternatives, 
+	 * putting the results into ranking matrix and sorting it by ranking points amount
+	 */
+	private void calculate_ranking() {
+		System.out.println(altCount_);
+		Integer[][] rankingM= new Integer[altCount_][altCount_];
+		
+		for (int a=0; a<altCount_; a++) {
+			for (int b=0; b<altCount_; b++){
+				if (a!=b) {
+					//concordance test
+					if ((concordance_index(a, b) > s_) && (discordance_condition( a,  b)))
+					{
+						rankingM[a][b]=1;
+					}
+					else 
+						rankingM[a][b]=0;
+				}
+				else 
+					rankingM[a][b]=0;
 			}
 		}
-
+//		for (int i=0; i<altCount_; i++){
+//			for (int j=0; j<altCount_; j++)
+//				System.out.print(rankingM[i][j]+" ");
+//			System.out.println();
+//		}
+		
+		//preparing map containing order number and ranking points
+		HashMap<Integer,Integer> rankMap = new HashMap<Integer,Integer>();
+		for (int a=0; a< altCount_; a++)
+			{
+			rankMap.put(a+1, 0);
+			for (int i=0; i<altCount_; i++) {
+				rankMap.put(a+1, rankMap.get(a+1)+rankingM[a][i]);
+			}
+		}
+		//sorting map by values - ranking points
         LinkedHashMap<Integer,Integer> sorted_map = sortHashMapByValuesD(rankMap);
-        ranking_=sorted_map.keySet().toArray(new Integer[alternatives_.size()]);
-        rankingPoints_ = sorted_map.values().toArray(new Integer[alternatives_.size()]);
+        ranking_=sorted_map.keySet().toArray(new Integer[altCount_]);
+        rankingPoints_ = sorted_map.values().toArray(new Integer[altCount_]);
 		
 	}
 	
-	public LinkedHashMap<Integer, Integer> sortHashMapByValuesD(HashMap<Integer, Integer> passedMap) {
+
+	/**
+	 * Method used for sorting maps by value
+	 * @param passedMap Integer map to sort by value
+	 * @return Integer map sorted by value
+	 */
+	private LinkedHashMap<Integer, Integer> sortHashMapByValuesD(HashMap<Integer, Integer> passedMap) {
 	    List<Integer> mapKeys = new ArrayList<Integer>(passedMap.keySet());
 	    List<Integer> mapValues = new ArrayList<Integer>(passedMap.values());
 	    Collections.sort(mapValues);
 	    Collections.sort(mapKeys);
 	    Collections.reverse(mapValues);
-
+	
 	    LinkedHashMap<Integer, Integer> sortedMap = 
 	        new LinkedHashMap<Integer, Integer>();
 	    
